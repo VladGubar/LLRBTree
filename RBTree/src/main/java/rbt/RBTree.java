7b7c58d48c24dc15aa90113f5158ae7dfa4e4b9d
waving666 <waving666@waving666-Lenovo-ideapad-320-15IKB>
2019-10-18 14:59:48 +0200

    Init commit

---
+++ RBTree/src/main/java/rbt/RBTree.java
@@ -0,0 +1,16 @@
+package rbt;
+
+
+public class RBTree<K extends Comparable<K>, V> implements MapInterface<K, V> {
+
+    @Override
+    public void setValue(K key, V value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public V getValue(K key) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
