0cb30f0edee43236a38571e76c3d99a9ef6bfe55
waving666 <waving666@waving666-Lenovo-ideapad-320-15IKB>
2019-10-18 16:15:20 +0200

    Added Node Class, Node and RBTree constructors

--- RBTree/src/main/java/rbt/RBTree.java
+++ RBTree/src/main/java/rbt/RBTree.java
@@ -2,7 +2,33 @@ package rbt;
 
 
 public class RBTree<K extends Comparable<K>, V> implements MapInterface<K, V> {
-
+    
+    static class Node<K, V> {
+        K key;
+        V value;
+        Node <K, V> leftChild;
+        Node <K, V> rightChild;
+        public enum Color {
+            Red,
+            Black;
+        }
+    
+    
+        public Node(K key, V value, enum Color) {
+                this.key = key;
+                this.value = value;
+                this.Color = Color;
+             }
+    }
+    
+    private Node<K, V> root;
+    private int size;
+    
+    public RBTree () {
+        size = 0;
+        root = null;
+    }
+    
     @Override
     public void setValue(K key, V value) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
